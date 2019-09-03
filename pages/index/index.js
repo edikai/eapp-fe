ed0ed253e7646fb5e0dc773ebb738115afabe60f
edikai <edi_kai@163.com>
2019-09-03 23:07:32 +0800

    m

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,42 @@
+import * as login from './../../utils/login.js';
+
 let app = getApp();
 
 Page({
+  data: {
+    toView: 'red',
+    scrollTop: 100,
+  },
+  upper(e) {
+    console.log(e);
+  },
+  lower(e) {
+    console.log(e);
+  },
+  scroll(e) {
+    console.log(e.detail.scrollTop);
+  },
+  scrollToTop(e) {
+    console.log(e);
+    this.setData({
+      scrollTop: 0,
+    });
+  },
   onLoad(query) {
     // 页面加载
     console.info(`Page onLoad with query: ${JSON.stringify(query)}`);
     console.log(app)
+    let token = app.globalData.accessToken
+    if(!token) {
+      console.log('mini project is not init.')
+      this.getAccessToken()
+    }else{
+      this.getMtBaseList()
+    }
   },
   onReady() {
     // 页面加载完成
+    
   },
   onShow() {
     // 页面显示
@@ -35,20 +64,96 @@ Page({
       path: 'pages/index/index',
     };
   },
-  formSubmit: function(e) {
-      let that = this;
-      console.log('form发生了submit事件，携带数据为：');
+  getAccessToken: function(e) {
+    let _self = this;
+    console.log('获取access-token');
+    dd.httpRequest({
+      url: login.getUrl()+'/get-token',
+      method: 'POST',
+      data: JSON.stringify({
+      }),
+      headers:{'Content-Type': 'application/json', 'login_name': 'admin', 'password': 'itc123', 'corp_id': app.globalData.corpId},
+      dataType: 'json',
+      success: (res) => {
+        // dd.alert({content: "res: " + JSON.stringify(res)});
+        console.log('get-token res: ', res)
+        if(res.status == 200 && res.data.code == 200){
+          app.globalData.accessToken = 'Bearer ' + res.data.token
+          _self.ddGetAuthCode(app)
+        }
+        
+      },
+      fail: (res) => {
+        console.log("httpRequestFail---",res)
+        dd.alert({content: JSON.stringify(res)});
+      },
+      complete: (res) => {
+        dd.hideLoading();
+      }
+    })
+  },
+  ddGetAuthCode: function(e) {
+    let _self = this
+    dd.getAuthCode({
+      success:(res)=>{
+        dd.httpRequest({
+          url: login.getUrl()+'/login',
+          method: 'POST',
+          async: false,
+          data: {
+              authCode: res.authCode
+          },
+          dataType: 'json',
+          headers: {'Authorization': app.globalData.accessToken},
+          success: (res) => {
+              console.log('success----',res)
+              if(res.status == 200 && res.data.code == 200){
+                app.globalData.userId = res.data.userId
+                app.globalData.userName = res.data.userName
+                app.globalData.deptId = res.data.deptId
+                // result = res.data
+                _self.getMtBaseList()
+              }
+          },
+          fail: (res) => {
+              console.log("httpRequestFail---",res)
+              dd.alert({content: JSON.stringify(res)});
+          },
+          complete: (res) => {
+              dd.hideLoading();
+          }
+        });
+      },
+      fail: (err)=>{
+          dd.alert({content: "step3"});
+          dd.alert({
+              content: JSON.stringify(err)
+          })
+      }
+    })
+  },
+  getMtBaseList: function(e) {
+      let _self = this;
+      console.log('获取建材列表数据');
       dd.httpRequest({
-        url: app.globalData.url+'/api/get-token',
+        url: login.getUrl()+'/mt-base-info/query-all',
         method: 'POST',
         data: JSON.stringify({
-            login_name: "admin",
-            Authorization: "xxxxxxxxxxxxxxxxxxxxxxx",
         }),
-        headers:{'Content-Type': 'application/json', 'login_name': 'admin', 'password': 'itc123', 'user_id': app.globalData.userId},
+        headers:{'Content-Type': 'application/json', 'Authorization': app.globalData.accessToken},
         dataType: 'json',
         success: (res) => {
-            dd.alert({content: "res: " + JSON.stringify(res)});
+          console.log("getMtBaseList = ", res)
+          if(res.status == 200 && res.data.code == 200){
+            console.log(res)
+          }else if(res.status == 200) {
+            console.log(res)
+            let msg = res.data.msg
+            console.log(msg)
+            dd.alert(msg);
+          }else {
+            JSON.stringify(res)
+          }
         },
         fail: (res) => {
             console.log("httpRequestFail---",res)
---
+++ utils/login.js
@@ -0,0 +1,82 @@
+const const_url = 'http://127.0.0.1:8080/api'
+
+export function ddGetAuthCode(app) {
+  let _self = this
+  let result = {}
+  dd.getAuthCode({
+    success:(res)=>{
+      dd.httpRequest({
+        url: const_url+'/login',
+        method: 'POST',
+        async: false,
+        data: {
+            authCode: res.authCode
+        },
+        dataType: 'json',
+        headers: {'Authorization': app.globalData.accessToken},
+        success: (res) => {
+            console.log('success----',res)
+            if(res.status == 200 && res.data.code == 200){
+              app.globalData.userId = res.data.result.userId
+              app.globalData.userName = res.data.result.userName
+              app.globalData.deptId = res.data.result.deptId
+              // result = res.data
+            }
+        },
+        fail: (res) => {
+            console.log("httpRequestFail---",res)
+            dd.alert({content: JSON.stringify(res)});
+        },
+        complete: (res) => {
+            dd.hideLoading();
+        }
+      });
+    },
+    fail: (err)=>{
+        dd.alert({content: "step3"});
+        dd.alert({
+            content: JSON.stringify(err)
+        })
+    }
+  })
+
+  return result
+}
+
+export function getAccessToken(app) {
+  let _self = this;
+  console.log('获取access-token');
+  dd.httpRequest({
+    url: const_url+'/get-token',
+    method: 'POST',
+    async: false,
+    data: JSON.stringify({
+    }),
+    headers:{'Content-Type': 'application/json', 'login_name': 'admin', 'password': 'itc123', 'corp_id': app.globalData.corpId},
+    dataType: 'json',
+    success: (res) => {
+      // dd.alert({content: "res: " + JSON.stringify(res)});
+      console.log('get-token res: ', res)
+      if(res.status == 200 && res.data.code == 200){
+        app.globalData.accessToken = 'Bearer ' + res.data.token
+        ddGetAuthCode(app)
+      }
+      
+    },
+    fail: (res) => {
+      console.log("httpRequestFail---",res)
+      dd.alert({content: JSON.stringify(res)});
+    },
+    complete: (res) => {
+      dd.hideLoading();
+    }
+  })
+}
+
+function getUrl() {
+  return const_url
+}
+
+export {
+  getUrl
+}
